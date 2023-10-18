b2929c7d3f79c5a5cccd5f09ef417ce85e56e5a3
VictorJ-Ch <chavarria.martinez.victor101@gmail.com>
2023-10-18 11:29:18 -0600

    Murio el proyecto

--- docs/JS/main.js
+++ docs/JS/main.js
@@ -1,29 +1,39 @@
-    //Bttn
-    document.addEventListener('DOMContentLoaded', function() {
-        const returnBttn = document.getElementById('returnBttn');
-        if (returnBttn) {
-            returnBttn.addEventListener('click', function() {
-                window.location.href = '../../index.html';
-            });
-        }
-    });
-    // Función para cambiar el tema
-    function toggleTheme() {
-        const themeLink = document.getElementById('theme-stylesheet');
-        if (themeLink.getAttribute('href') === '../CSS/light.css') {
-            themeLink.setAttribute('href', '../CSS/dark.css');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            themeLink.setAttribute('href', '../CSS/light.css');
-            localStorage.setItem('theme', 'light');
-        }
+// Función para cambiar el tema en index.html
+function toggleTheme() {
+    const themeLink = document.getElementById('theme-stylesheet');
+    if (themeLink.getAttribute('href') === './CSS/light.css') {
+        themeLink.setAttribute('href', './CSS/dark.css');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        themeLink.setAttribute('href', './CSS/light.css');
+        localStorage.setItem('theme', 'light');
     }
-    // Agregar un evento al botón
-        const themeToggleBtn = document.getElementById('theme-toggle');
-        themeToggleBtn.addEventListener('click', toggleTheme);
-
-    // Comprobar si se ha seleccionado un tema previamente
-        const selectedTheme = localStorage.getItem('theme');
-        if (selectedTheme === 'dark') {
-            toggleTheme(); // Cambiar al tema oscuro si estaba seleccionado
-        }
+}
+// Agregar un evento al botón
+const themeToggleBtn = document.getElementById('theme-toggle');
+themeToggleBtn.addEventListener('click', toggleTheme);
+// Comprobar si se ha seleccionado un tema previamente
+const selectedTheme = localStorage.getItem('theme');
+if (selectedTheme === 'dark') {
+    toggleTheme(); // Cambiar al tema oscuro si estaba seleccionado
+}
+//OTRO
+// Función para cambiar el tema en index.html
+function toggleTheme2() {
+    const themeLink = document.getElementById('theme-stylesheet');
+    if (themeLink.getAttribute('href') === '../CSS/light.css') {
+        themeLink.setAttribute('href', '../CSS/dark.css');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        themeLink.setAttribute('href', '../CSS/light.css');
+        localStorage.setItem('theme', 'light');
+    }
+}
+// Agregar un evento al botón
+const themeToggleBtn2 = document.getElementById('theme-toggle2');
+themeToggleBtn.addEventListener('click', toggleTheme2);
+// Comprobar si se ha seleccionado un tema previamente
+const selectedTheme2 = localStorage.getItem('theme');
+if (selectedTheme === 'dark') {
+    toggleTheme(); // Cambiar al tema oscuro si estaba seleccionado
+}
